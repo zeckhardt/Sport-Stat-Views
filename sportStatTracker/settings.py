25d79b94b91aac3b8acaebe62e0e42236e5ba8aa
Zach Eckhardt <zre7677@rit.edu>
2024-09-23 10:39:41 -0400

    another fix

--- sportStatTracker/settings.py
+++ sportStatTracker/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
